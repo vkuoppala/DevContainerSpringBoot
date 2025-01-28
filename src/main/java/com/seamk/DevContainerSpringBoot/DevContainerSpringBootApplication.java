f223111c29982858d5bd529b7ba35162da3a1a79
Ville <ville.a.kuoppala@seamk.fi>
2025-01-28 12:30:12 +0200

    lisätty springboot
    lisätty springin tiedostot

---
+++ src/main/java/com/seamk/DevContainerSpringBoot/DevContainerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.seamk.DevContainerSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevContainerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevContainerSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seamk/DevContainerSpringBoot/DevContainerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seamk.DevContainerSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevContainerSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
